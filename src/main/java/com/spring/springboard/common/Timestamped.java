8d3b42bd6450d2a2dd5aa0c1416353637f883877
kikye04040 <kikye04040@gmail.com>
2024-11-19 19:27:06 +0900

    feat: Timestamped 구현

---
+++ src/main/java/com/spring/springboard/common/Timestamped.java
@@ -0,0 +1,24 @@
+package com.spring.springboard.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+    @CreatedBy
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
