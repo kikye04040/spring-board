d1f3ba95f1227750b5b00958fe7020c74a1994a6
kikye04040 <kikye04040@gmail.com>
2024-11-19 19:27:22 +0900

    feat: Board 엔티티 구현

---
+++ src/main/java/com/spring/springboard/board/entity/Board.java
@@ -0,0 +1,23 @@
+package com.spring.springboard.board.entity;
+
+import com.spring.springboard.common.Timestamped;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "boards")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+}
