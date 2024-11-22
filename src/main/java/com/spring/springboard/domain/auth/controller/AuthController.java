b0f2fc17854809643ada60a6bc6b6caa9425cc14
kikye04040 <kikye04040@gmail.com>
2024-11-22 16:50:47 +0900

    feat(user): 컨트롤러 구현

--- src/main/java/com/spring/springboard/domain/auth/controller/AuthController.java
+++ src/main/java/com/spring/springboard/domain/auth/controller/AuthController.java
@@ -1,11 +1,39 @@
 package com.spring.springboard.domain.auth.controller;
 
+import com.spring.springboard.domain.auth.dto.request.AdminSignupRequest;
+import com.spring.springboard.domain.auth.dto.request.SigninRequest;
+import com.spring.springboard.domain.auth.dto.request.SignupRequest;
+import com.spring.springboard.domain.auth.dto.response.SigninResponse;
+import com.spring.springboard.domain.auth.dto.response.SignupResponse;
 import com.spring.springboard.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/auth/signup")
+    public SignupResponse signup(@Valid @RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
+
+    @PostMapping("/auth/signin")
+    public com.spring.springboard.domain.auth.dto.response.SigninResponse signin(@Valid @RequestBody SigninRequest request) {
+        return authService.signin(request);
+    }
+
+    @PostMapping("/admin/signup")
+    public SignupResponse adminSignup(@Valid @RequestBody AdminSignupRequest request) {
+        return authService.adminSignup(request);
+    }
+
+    @PostMapping("/admin/signin")
+    public SigninResponse adminSignin(@Valid @RequestBody SigninRequest request) {
+        return authService.adminSignin(request);
+    }
 }
