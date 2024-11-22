3986b4dbfc2e0e5351e7026e371bd93cbdea89dc
kikye04040 <kikye04040@gmail.com>
2024-11-22 16:55:59 +0900

    feat(user): dto 구현

---
+++ src/main/java/com/spring/springboard/domain/auth/dto/request/AdminSignupRequest.java
@@ -0,0 +1,30 @@
+package com.spring.springboard.domain.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminSignupRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @NotBlank
+    private String adminKey;
+}
