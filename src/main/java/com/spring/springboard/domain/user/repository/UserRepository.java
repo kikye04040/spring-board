02add26989a545e07ecca4049cd3caa05c7a9022
kikye04040 <kikye04040@gmail.com>
2024-11-23 23:02:00 +0900

    feat(user): 이메일로 유저 찾기 구현

---
+++ src/main/java/com/spring/springboard/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.springboard.domain.user.repository;
+
+import com.spring.springboard.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+}
