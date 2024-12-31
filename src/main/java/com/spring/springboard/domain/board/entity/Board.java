d2d279c259b63d575cdce073455d4258a9ed8828
kikye04040 <kikye04040@gmail.com>
2025-01-01 00:59:57 +0900

    feat(board): 엔티티 구현

--- src/main/java/com/spring/springboard/domain/board/entity/Board.java
+++ src/main/java/com/spring/springboard/domain/board/entity/Board.java
@@ -1,15 +1,28 @@
 package com.spring.springboard.domain.board.entity;
 
+import com.spring.springboard.domain.attachment.entity.Attachment;
+import com.spring.springboard.domain.board.enums.BoardStatus;
+import com.spring.springboard.domain.category.entity.Category;
+import com.spring.springboard.domain.comment.entity.Comment;
 import com.spring.springboard.domain.common.entity.Timestamped;
+import com.spring.springboard.domain.tag.entity.Tag;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "boards")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE boards SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Board extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +33,47 @@ public class Board extends Timestamped {
 
     @Column(nullable = false, length = 100)
     private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BoardStatus status;
+
+    @Column(nullable = false)
+    private int viewCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Attachment> attachments = new ArrayList<>();
+
+    @Column(nullable = false)
+    private int likeCount;
+
+    @Column(nullable = false)
+    private int dislikeCount;
+
+    @ManyToMany
+    @JoinTable(
+            name = "board_tags",
+            joinColumns = @JoinColumn(name = "board_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Tag> tags = new ArrayList<>();
+
+    public void incrementViewCount() {
+        this.viewCount++;
+    }
+
+    public void incrementLikeCount() {
+        this.likeCount++;
+    }
+
+    public void incrementDislikeCount() {
+        this.dislikeCount++;
+    }
 }
