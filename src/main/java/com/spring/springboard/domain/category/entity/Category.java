64a85a8848f8d5961e80591ca8692270c9f45594
kikye04040 <kikye04040@gmail.com>
2025-01-01 01:00:01 +0900

    feat(board): 엔티티 구현

--- src/main/java/com/spring/springboard/domain/category/entity/Category.java
+++ src/main/java/com/spring/springboard/domain/category/entity/Category.java
@@ -1,4 +1,26 @@
 package com.spring.springboard.domain.category.entity;
 
+import com.spring.springboard.domain.board.entity.Board;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Board> boards = new ArrayList<>();
 }
