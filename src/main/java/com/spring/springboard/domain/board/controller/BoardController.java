7b3a5bacd13bda9883371d48a8ef7d975ae6b669
kikye04040 <kikye04040@gmail.com>
2025-01-01 21:52:06 +0900

    feat(board): createBoard 로직 구현

--- src/main/java/com/spring/springboard/domain/board/controller/BoardController.java
+++ src/main/java/com/spring/springboard/domain/board/controller/BoardController.java
@@ -1,7 +1,17 @@
 package com.spring.springboard.domain.board.controller;
 
+import com.spring.springboard.domain.board.dto.request.BoardRequest;
+import com.spring.springboard.domain.board.dto.response.BoardResponse;
+import com.spring.springboard.domain.board.entity.Board;
 import com.spring.springboard.domain.board.service.BoardService;
+import com.spring.springboard.domain.user.entity.CustomUserDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/boards")
 public class BoardController {
     private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<BoardResponse> createBoard(
+            @RequestBody @Valid BoardRequest request,
+            @AuthenticationPrincipal CustomUserDetails authUser) {
+        Board board = boardService.createBoard(request, authUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new BoardResponse(board));
+    }
 }
--- src/main/java/com/spring/springboard/domain/board/service/BoardService.java
+++ src/main/java/com/spring/springboard/domain/board/service/BoardService.java
@@ -1,6 +1,13 @@
 package com.spring.springboard.domain.board.service;
 
+import com.spring.springboard.domain.board.dto.request.BoardRequest;
+import com.spring.springboard.domain.board.entity.Board;
 import com.spring.springboard.domain.board.repository.BoardRepository;
+import com.spring.springboard.domain.common.enums.ErrorStatus;
+import com.spring.springboard.domain.common.exception.ApiException;
+import com.spring.springboard.domain.user.entity.CustomUserDetails;
+import com.spring.springboard.domain.user.entity.User;
+import com.spring.springboard.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +15,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+
+    public Board createBoard(BoardRequest request, CustomUserDetails authUser) {
+        User author = userRepository.findByEmail(authUser.getEmail())
+                .orElseThrow(() -> new ApiException(ErrorStatus.NOT_FOUND_USER));
+
+        Board board = new Board(request.getTitle(), request.getDescription(), author);
+
+        return boardRepository.save(board);
+    }
 }
